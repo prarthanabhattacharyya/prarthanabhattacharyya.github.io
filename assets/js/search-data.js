aee8f771297b75143d0af078762b47d4fe25c889
AutoVision-cloud <66959312+AutoVision-cloud@users.noreply.github.com>
2026-03-13 12:12:58 +0000

    Deploying to gh-pages from @ prarthanabhattacharyya/prarthanabhattacharyya.github.io@05387d2fd435ff5e608210d864189b43d326bd2f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
